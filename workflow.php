ae9fa15d67e21e2f54949d3a4a1e4a9e04447b7d
gpullman <gpullman@gsu.edu>
2016-03-02 09:27:58 -0500

    Initial commit

---
+++ workflow.php
@@ -0,0 +1,47 @@
+<h1>Workflow</h1>
+<p>English 2105 is a class in research-based problem solving and writing, where research means reading recent publications, surveying, interviewing, observing, and testing the efficacy of potential solutions. The problems you solve don't have ready made solutions. There are no formulas. There are unanswerable questions and therefore unsolvable problems. Part of the task is to identify and properly frame a situation as a problem before seeking and testing possible solutions. No expert exists to solve these problems. You have to become the expert. You have to convince relevant others that there is a problem and that you have what it takes to solve it.
+
+<p>This workflow is the essence of being an effective member of a community, and it is the means by which you can obtain an invitation to new communities: This is the kind of thinking and writing that gets you paid.
+
+<ol>
+<li>Identify a problem (correct a mistake, improve a condition or tool) or identify an opportunity (innovate, invent) 
+
+<dd>What constitutes a problem? (pain, loss of something valued -- time, money, productivity, efficiency, work/life balance, etc. )</dd>
+<dd>What constitutes an opportunity? (unmet demand, unadentified demand)
+<dd>What kinds of problems are there? (probabilistic, deterministic, wicked)</dd>
+<dd>What constitutes a pseudo problem: errors in thinking, in perception, in context  </dd>
+
+<li>Frame the problem -- who, why, what, where, when, how
+
+<li>Do the research
+<ol>
+<li>Read to find <dd>existing solutions or previous attempts</dd> <dd>discover relevant background</dd>
+<dd>find out what others (your competitors perhaps) have done or are doing</dd>
+<li>Conduct survey(s) to refine understanding of percieved problem
+<li>Conduct interviews to identify perceptions of the problem (prove it exists, is a problem, is worth solving)
+<li>Conduct contextual observations to verify user preception
+ 
+<dd>errors in execution</dd>
+
+
+<li>Create a list of possible solutions
+<dd>What constitutes a solution? What kinds are there?</dd>
+<dd>Erroneous solutions (too costly, unsustainable, too narrow, unintended consequences, deal with symptom not disease)</dd>
+
+<li>Develop a way to test the efficacy of each solution
+
+
+<li>Pitch the best solution
+<dd>3 sides, 7 minutes</dd>
+
+</ol>
+
+<p>EG: You work for the library. You are a student. You hear students complaining about having nowhere to work. You observe kids camped out on the floor next to outlets intended for linoleum polishing. through interviews you discover that the rooms provided for study seem always to be booked. you conduct a survey to discover if a digital signup system for scheduling the rooms would help students. You discover that it would enable the library to better prove effective allocation of resources (so you know they will pay for the development), but you also discover that students tend to want the spaces at the same times of day, making scheduling only a bit helpful than first come first served. Should you pitch the app solution or look for alternatives? Consider thinking outside the library?
+
+<p>EG: You work for a company that runs several taverns in Atlanta. Data indicates that one store goes through more liquor than the others, but shows only slightly more profit for liquor sales. Since you are known to the employees, you enlist an informant to go to several of the bars and make notes. How do you pick the informant (what traits do you want)? What do you ask the informant to do, what questions do they need to answer? Imagine you discover the source of the difference. Say what it is and what you think should be done about it. Remember that the problem is a mixed bag, increased profit but decreased efficiency.
+
+<p>EG: You work for a large realtor. You discover this article about rising sea levels. What do you do and why? What don't you do and why?  
+
+<p>You've read a rumor that the earphone jack will be eliminated in next generation phones. What do you do? What do you recommend? To whom? 
+
+<p>EG: Some problems are hard to resolve because the people who have the power and resources to implement the solution don't have the problem. Worse yet,  the solution might seem to create a problem for them. In those cases you need to figure out how to re-frame the situation to answer the what's-in-it-for-them question. You have to figure out how to see the world from their perspective.
